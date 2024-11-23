01146621051fce557eb3baf02922d0d255dcf336
marcialf <marcial.fernandez@uece.br>
2024-11-23 16:31:04 -0300

    include sudo

--- images/jupyter-hub/build.sh
+++ images/jupyter-hub/build.sh
@@ -5,7 +5,7 @@ VERSION="1.0"
 set -e
 
 ## Build Docker image
-docker build -t lasid/jupyter-hub:${SPARK_VER}_${UBUNTU_VER} \
+sudo docker build -t lasid/jupyter-hub:${SPARK_VER}_${UBUNTU_VER} \
              -t lasid/jupyter-hub:latest .
 
 ## Push image to DockerHub
