b94a25b2ff87aa85b6bf0b636c58889b2b8bad01
Marcial Fernandez <marcial.fernandez@uece.br>
2024-11-23 11:14:40 -0300

    Update build.sh

--- images/jupyter-hub/build.sh
+++ images/jupyter-hub/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
-UBUNTU_VER="focal"
-
+UBUNTU_VER="jammy"
+VERSION="1.0"
 set -e
 
 ## Build Docker image
@@ -10,12 +9,12 @@ docker build --build-arg SP_VERSION=${SPARK_VER} --build-arg JV_VERSION=${JAVA_V
              --build-arg HD_VERSION=${HADOOP_VER} --build-arg SP_G_PKG=${SPARK_GRAPH_PKG} --build-arg SP_D_PKG=${SPARK_DELTA_PKG} \
              -t lasid/spark-base:${SPARK_VER}_${UBUNTU_VER}  -t lasid/spark-base:latest ./docker/spark-base
 docker build --build-arg SP_VERSION=${SPARK_VER} -t lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER} \
-             -t lasid/spark-worker:latest ./docker/spark-worker
+             -t lasid/jupyter-hub:latest .
 
 ## Push image to DockerHub
 docker login
-#docker push lasid/spark-worker:latest
-docker push lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER}
+#docker push lasid/jupyter-hub:latest
+docker push lasid/jupyter-hub:${VERSION}_${UBUNTU_VER}
 
 
 
